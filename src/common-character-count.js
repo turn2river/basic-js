76de7e4fe415e4d16b1df4f6d7b1a29d2f63785e
Alexey Krylov <turn2river@gmail.com>
2023-04-13 12:00:02 +0300

    feat: impement solution for cats and character count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -11,9 +9,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1,s2) {
+  let counter = 0
+  let a1 = s1.split('')
+  let a2 = s2.split('')
+
+  for ( let i = 0; i < a1.length; i++) {
+      if (a2.indexOf(a1[i]) > -1) {
+        a2.splice(a2.indexOf(a1[i]),1)
+        counter++
+      }
+  }
+  return counter
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
+  let counter = 0
+  for (i in matrix) {
+    matrix[i].forEach(e => {
+      if (e === '^^') counter++
+    })
+  }
+  return counter
+
+
 }
 
 module.exports = {
