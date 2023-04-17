86c8020deec89e9fe3271a9d317fed589176427f
alexey.krylov <turn2river@gmail.com>
2023-04-17 10:10:51 +0300

    reaf: add solution for carbon,delete-digit,season

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let target = Infinity
+  let arr = n.toString().split('')
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[i+1]) {
+      target = arr[i]
+    }
+  }
+  arr.splice(arr.indexOf(target),1)
+  return +arr.join('')
 }
-
 module.exports = {
   deleteDigit
 };
--- src/what-season.js
+++ src/what-season.js
@@ -2,18 +2,34 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+  try {
+    date.getTime()
+  } catch {
+    throw new Error("Invalid date!")
+  }
+  if (date.getMonth() > 1 && date.getMonth() < 5) {
+    return 'spring'
+  }
+  if (date.getMonth() > 4 && date.getMonth() < 8) {
+    return 'summer'
+  }
+  if (date.getMonth() > 7 && date.getMonth() < 11) {
+    return 'autumn'
+  }
+  else { return 'winter'}
 }
 
 module.exports = {
