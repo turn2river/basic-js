99779ae6bfebaabe1c21784a7d7cc682b622ca76
alexey.krylov <turn2river@gmail.com>
2023-04-18 00:43:26 +0300

    reaf: add solutions for simple-chain, extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,50 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str,{
+  repeatTimes = 1,
+  separator = '+',
+  addition = '',
+  additionRepeatTimes = 1,
+  additionSeparator = '|',
+}) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let arr1 = []
+  let arr2 = []
+  str = String(str)
+
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    if (addition === null) {
+      addition = String(addition)
+    }
+    arr1.push(addition)
+  }
+
+  arr1 = arr1.join(additionSeparator)
+
+
+  str = str + arr1
+
+  for (let i = 0; i < repeatTimes; i++) {
+    arr2.push(str)
+  }
+  return arr2.join(separator)
+
 }
 
 module.exports = {
